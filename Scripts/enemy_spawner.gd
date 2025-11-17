64e95217bb9ed4e4aa2cc8369bf4c14593487674
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-11-17 15:22:10 +0100

    base pour enemy spawner

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -2,4 +2,30 @@ extends Node3D
 
 class_name EnemySpawner
 
-@export var enemy_scene : Array[PackedScene]
+@export var enemy_scenes : Array[PackedScene]
+@export var spawn_interval : Array[Vector2]
+@export var min_spawn_distance : int = 20
+@export var max_spawn_distance : int = 50
+
+@onready var spawn_timer =$SpawnTimer
+
+var player : Player
+var rng = RandomNumberGenerator.new()
+
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	spawn_timer.start(randf_range(spawn_interval.get(0).x,spawn_interval.get(0).y))
+	spawn_timer.timeout.connect(spawn_enemy)
+
+func spawn_enemy() -> void :
+	
+	var distance : int = rng.randi_range(min_spawn_distance, max_spawn_distance)
+	
+	var x = 1 + rng.randf_range(-1, 1)
+	var z = 1 + rng.randf_range(-1, 1)
+	
+	var enemy = enemy_scenes.get(0).instantiate()
+	
+	get_parent().add_child(enemy)
+	enemy.position = player.global_position + (distance * Vector3(x,0.0,z).normalized())
