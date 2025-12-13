32219e7817a3eb21a29f339b77067b57ac7e0e6d
blanchard960 <elyne.derocles@laposte.net>
2025-12-13 20:32:11 +0100

    adaptation du gameManager avec menu d'accueil

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -4,17 +4,21 @@ var current_score: int =0
 var is_game_running = true
 
 
-func increase_score(score) :
+func increase_score(score)-> void  :
 	current_score += score
 	print("score : ",current_score)
 
-func game_over():
+func game_over()-> void :
 	#var uimanager : UIManager = get_tree().get_first_node_in_group("ui_manager")
 	#uimanager.display_end_buttons(true)
 	is_game_running = false
 	restart()
 
-func restart():
+func restart()-> void :
 	get_tree().reload_current_scene()
 	is_game_running = true
 	current_score = 0
+
+
+func quit() -> void :
+	get_tree().quit()
