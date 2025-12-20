fd0f0946a0d614aa87f8715d9874924ab130b644
blanchard960 <elyne.derocles@laposte.net>
2025-12-20 15:55:16 +0100

    augmentation de la difficultée

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,9 +2,12 @@ extends Node
 
 var current_score: int =0
 var is_game_running = true
-var difficulty_increase_interval = 10
+@export var difficulty_increase_interval = 10
+@export var spawn_interval_orig : Array[Vector2] = [Vector2(1,2),Vector2(2,3)]
+@export var spawn_number_orig : Array[float] = [1,1]
 
-@export var spawn_interval : Array[Vector2] = [Vector2(1,2),Vector2(2,3)]
+var spawn_interval : Array[Vector2] = spawn_interval_orig
+var spawn_number : Array[float] = spawn_number_orig
 @onready var main_scene : PackedScene = preload("res://Scene/main_scene.tscn")
 @onready var game_over_scene : PackedScene = preload("res://Scene/UI/game_over_menu.tscn")
 @onready var difficulty_timer : Timer
@@ -12,6 +15,7 @@ var difficulty_increase_interval = 10
 
 func start_difficulty_timer()-> void :
 	difficulty_timer = get_tree().get_first_node_in_group("difficulty_timer")
+	difficulty_timer.timeout.connect(increase_difficulty)
 	difficulty_timer.start(difficulty_increase_interval)
 
 func increase_score(score)-> void  :
@@ -19,22 +23,29 @@ func increase_score(score)-> void  :
 
 func game_over()-> void :
 	is_game_running = false
+	spawn_interval = spawn_interval_orig
+	spawn_number = spawn_number_orig
 	get_tree().change_scene_to_packed(game_over_scene)
 	get_tree().reload_current_scene()
 
 
 func restart()-> void :
-	get_tree().change_scene_to_packed(main_scene)
-	get_tree().reload_current_scene()
 	is_game_running = true
 	current_score = 0
+	get_tree().change_scene_to_packed(main_scene)
+	get_tree().reload_current_scene()
 
 
 func quit() -> void :
 	get_tree().quit()
 
 func increase_difficulty() -> void:
-	var spawn_interval_temp = []
+	var spawn_interval_temp : Array[Vector2] = []
+	var spawn_number_temp : Array[float] = []
 	for i in spawn_interval.size():
-		spawn_interval_temp.append(spawn_interval.get(i)*0.5)
+		spawn_interval_temp.append(spawn_interval.get(i)-spawn_interval.get(i)*0.01)
+	for i in spawn_number.size():
+		spawn_number_temp.append(spawn_number.get(i)+0.5)
+	spawn_interval = spawn_interval_temp
+	spawn_number = spawn_number_temp
 	return
