95c903e6d68257e0de97778a6711efe37c852a9b
blanchard960 <elyne.derocles@laposte.net>
2025-12-21 11:18:06 +0100

    ajout d'un life holder

---
+++ Scripts/life_holder.gd
@@ -0,0 +1,13 @@
+extends HBoxContainer
+
+@export var life_icon_scene : PackedScene
+@onready var player: Player = $"../../Player"
+
+func _ready() -> void:
+	for i in range(player.current_life):
+		var life_icon = life_icon_scene.instantiate()
+		add_child(life_icon)
+
+func _process(delta : float) -> void :
+	if player.current_life != get_child_count():
+		get_child((get_child_count()-1)).queue_free()
