44f15989ae6e29cfe224edd2a0613dbf9940975a
blanchard960 <elyne.derocles@laposte.net>
2025-12-21 14:23:08 +0100

    ajout nouvelle attaque

--- Scripts/player_zone_attack_1.gd
+++ Scripts/player_zone_attack_1.gd
@@ -1,1 +1,22 @@
 extends Area3D
+
+var player : Player
+var move : Vector3
+
+@export var damage = 1
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	global_position = player.global_position
+	animation_player.play("grow")
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is Enemy :
+		(body as Enemy).take_damage(damage)
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "grow" :
+		queue_free()
